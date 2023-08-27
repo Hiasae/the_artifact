c6e6c0e87fbd56dabe0ccdeb1c4cccb26281bb75
Hiasae <16280810+Hiasae@users.noreply.github.com>
2023-08-27 16:56:37 +0200

    updated player movement
    -updated frame animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+@onready var sprite_2d : Sprite2D = $Sprite2D
+var anim_update : float = 0.2
+var anim_time : float = 0
+var frame_y : int
+var frame_x : int
 
 const SPEED : float = 40
 
@@ -12,3 +17,24 @@ func _physics_process(delta):
 		velocity = Vector2.ZERO
 
 	move_and_slide()
+	update_sprite_animation(direction, delta)
+
+
+func update_sprite_animation(dir : Vector2, delta : float):
+	if not dir:
+		sprite_2d.frame_coords = Vector2i(frame_x,0)
+		return
+	anim_time += delta
+	if anim_time > anim_update:
+		anim_time = 0
+		frame_y = (frame_y + 1)%3
+		
+	frame_x = 2*int(dir.y < 0) 
+	if abs(dir.x) > 0:
+		frame_x = 3*int(dir.x < 0) + int(dir.x > 0)
+
+	sprite_2d.frame_coords = Vector2i(frame_x,frame_y)
+	
+	
+	
+	
